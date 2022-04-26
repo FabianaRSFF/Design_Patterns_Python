f77e9c14aebb9c961d18055facf33f7a72111966
Fabiana <fabianarsantos@gmail.com>
2022-04-26 11:41:01 -0300

    MonoStateSimple

--- monostate/monostate_1.py
+++ monostate/monostate_1.py
@@ -1,6 +1,9 @@
 # Monostate ou Borg - É uma variação do singleton para 
 # garantir que
 # estado do objeto seja igual para todas as instâncias.
+from pip import main
+
+
 class StringReprMixin:
     def __str__(self):
         params = ', '.join([f'{k}={v}' for k, v in self.__dict__.items()])
@@ -10,15 +13,22 @@ class StringReprMixin:
         return self.__str__()
 
 
-class A(StringReprMixin):
-    def __init__(self, nome):
-        self.x = 10
-        self.y = 20
-        self.nome = nome
+class MonoStateSimple(StringReprMixin):
+    _state = {
+        'x': 10,
+        'y': 20,
+    }
 
-    def __str__(self):
-        params = ', '.join([f'{k}={v}' for k, v in self.__dict__.items()])
-        return f'{self.__class__.__name__}({params})'
+    def __init__(self):
+        self.x = 1
+        self.__dict__ = self._state
 
-a = A('Luiz')
-print(a)
+if __name__ == '__main__':
+    m1 = MonoStateSimple()
+    m2 = MonoStateSimple()
+    print(m1)
+    print(m2)
+    # Se  você muda o valor em uma classe, o valor muda nas duas.
+    m1.x = 'qualquer coisa'
+    print(m1)
+    print(m2)
