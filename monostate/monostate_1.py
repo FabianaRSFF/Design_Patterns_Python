9e591c1a0b0f714632222004237a0aabf02db5ab
Fabiana <fabianarsantos@gmail.com>
2022-04-26 11:31:42 -0300

    StringReprMixin

--- monostate/monostate_1.py
+++ monostate/monostate_1.py
@@ -1,8 +1,16 @@
 # Monostate ou Borg - É uma variação do singleton para 
 # garantir que
 # estado do objeto seja igual para todas as instâncias.
+class StringReprMixin:
+    def __str__(self):
+        params = ', '.join([f'{k}={v}' for k, v in self.__dict__.items()])
+        return f'{self.__class__.__name__}({params})'
 
-class A:
+    def __repr__(self):
+        return self.__str__()
+
+
+class A(StringReprMixin):
     def __init__(self, nome):
         self.x = 10
         self.y = 20
