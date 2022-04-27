fdcc84ada535345542fe3d1d649db315a5caf302
Fabiana <fabianarsantos@gmail.com>
2022-04-27 10:07:57 -0300

    Strategy

---
+++ strategy/strategy.py
@@ -0,0 +1,69 @@
+# Padrão de projeto "comportamental" que tem a intenção de definir
+# uma família de algoritmos, encapsular cada uma delas e
+# torná-las intercambiáveis. Strategy permite que o algoritmo 
+# varie independentemente dos clientes que o utilizem.
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Order:
+    def __init__(self, total: float, discount: DiscountStrategy):
+        self._total = total
+        self._discount = discount
+    
+    @property
+    def total(self):
+        return self._total
+
+    @property
+    def total_with_discount(self):
+        return self._discount.calculate(self.total)
+
+
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def calculate(self, value: float) -> float: pass
+
+
+class TwentyPercent(DiscountStrategy):
+    def calculate(self, value: float) -> float:
+        return value - (value * 0.2)  
+
+class FiftyPercent(DiscountStrategy):
+    def calculate(self, value: float) -> float:
+        return value - (value * 0.5)  
+
+
+class NoDiscount(DiscountStrategy):
+    def calculate(self, value: float) -> float:
+        return value
+
+
+class CustomDiscount(DiscountStrategy):
+    def __init__(self, discount):
+        self.discount = discount / 100
+
+    def calculate(self, value: float) -> float:
+        return value - (value * self.discount)
+
+
+
+if __name__ == '__main__':
+    twenty_percent = TwentyPercent()
+    fifty_percent = FiftyPercent()
+    no_discount = NoDiscount()
+    custom_discount = CustomDiscount(13)
+
+    order = Order(1000, twenty_percent)
+    print(order.total, order.total_with_discount)
+
+    order = Order(1000, fifty_percent)
+    print(order.total, order.total_with_discount)
+
+    order = Order(1000, no_discount)
+    print(order.total, order.total_with_discount)
+
+    order = Order(1000, CustomDiscount(13))
+    print(order.total, order.total_with_discount)
+
+            
